f034c4adc75f34fb945228da51a9eb3f7110b20c
DutchNugget <232434153+DutchNugget@users.noreply.github.com>
2025-11-02 20:38:32 +0100

    created html placeholders and retrieved elements

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,10 @@
 
+const body = document.getElementById("square");
+const squareContainer = document.createElement("div");
+body.appendChild(squareContainer);
+
+
+function createSquareDiv (num) {
+    const square = document.createElement("div")
+    square
+}
