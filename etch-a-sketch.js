27e1a25a78bddcf1a44dd3477e80f674b6685a6c
DutchNugget <232434153+DutchNugget@users.noreply.github.com>
2025-11-02 21:21:09 +0100

    corrected function create num squares per side added basic styling to test logic

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,29 @@
 
 const body = document.getElementById("square");
 const squareContainer = document.createElement("div");
+squareContainer.classList.add("squareContainer")
 body.appendChild(squareContainer);
 
 
 function createSquareDiv (num) {
-    const square = document.createElement("div")
-    square
+    //total squares variable declared for function
+    // size declared for deciding width 
+    const totalSquares = num * num
+    const size = 100 / num
+    //for loop to create num correct amount of squares
+    for (let i = 0; i < totalSquares; i++){
+        const square = document.createElement("div")
+        square.classList.add("grid");
+        
+       
+        square.style.width = size + "%";
+        square.style.height = size + "%";
+
+        squareContainer.appendChild(square);
+    }
+   
+    
+    
 }
+
+createSquareDiv(20);
